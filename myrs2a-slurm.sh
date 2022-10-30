bce95e53c17d4879d9d1e92230365a595240cd7c
AWS ParallelCluster user <ec2-user@ip-10-0-9-23.ap-southeast-1.compute.internal>
2022-10-30 06:00:18 +0000

    lab7parta

--- myrs2a-slurm.sh
+++ myrs2a-slurm.sh
@@ -5,12 +5,12 @@
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --cpus-per-task=5	# number of processors per task
-#SBATCH -J "rs1a"   # job name
+#SBATCH --cpus-per-task=5      # number of processors per task
+#SBATCH -J "rs2a"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rs1a-slurm.%N.%j.out # STDOUT
-#SBATCH -e rs1a-slurm.%N.%j.err # STDERR
+#SBATCH -o rs2a-slurm.%N.%j.out # STDOUT
+#SBATCH -e rs2a-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
@@ -19,8 +19,8 @@ import os; \
 import time; \
 t0 = time.time(); \
 print(time.localtime()); \
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False); \
+DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(33,65)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False); \
 print(time.localtime()); \
 print(time.time()-t0);"
 
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:896152538583:awsnotify --message "RPLS1aJobDone"
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:896152538583:awsnotify --message "RPLS2aJobDone"
